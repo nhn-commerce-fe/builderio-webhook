22b904b933c2c99005b671c4cc1cc252ed12eb97
jenny-cho <jenny.cho@nhn.com>
2024-08-02 09:47:42 +0900

    chore: req 확인

--- src/pages/api/webhook/builderio.ts
+++ src/pages/api/webhook/builderio.ts
@@ -22,9 +22,28 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 //     return res.status(405).end(`Method ${req.method} Not Allowed`);
 //   }
 // }
-import { NextResponse } from 'next/server';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const doorayIncomingUrl = 'https://hook.dooray.com/services/1590498595903871702/3860379888550721054/7LG5oUm9QOqejchIWH4tlA';
+
+
+const callDoorayIncomingHook = async (params: any) => {
+  await fetch(doorayIncomingUrl, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    // body: '{"botName": "MyBot", "botIconImage": "https://static.dooray.com/static_images/dooray-bot.png", "text":"Dooray!"}',
+    body: JSON.stringify(
+      {
+        "botName": "BuilderIO 봇",
+        "botIconImage": "https://static.dooray.com/static_images/dooray-bot.png",
+        "text": "[WebHook Test Page] Publish"
+      } 
+  )
+});
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req, res);
 
   if (req.method === 'GET') {
@@ -47,6 +66,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
     // Respond to the request indicating success
     // https://hook.dooray.com/services/1590498595903871702/3860379888550721054/7LG5oUm9QOqejchIWH4tlA 두레이훅으로보내야함
+    await callDoorayIncomingHook(req);
     return res.status(200).json({ message: 'Webhook received and processed', req });
   } else {
 
